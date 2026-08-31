c4ae2516af4f10bf7739f40d9f40a6cb3c48c467
Didem Arslan <didemmrsln@gmail.com>
2026-08-31 21:09:42 +0300

    Completed Rice

--- farm/farming_diary.py
+++ farm/farming_diary.py
@@ -5,7 +5,7 @@ and checked for ripeness.
 """
 
 from farm.corn import Corn
-
+from farm.rice import Rice
 
 print("\n\n📝 Day One: Corn")
 
@@ -27,16 +27,19 @@ else:
 print("\n\n📝 Day Two: Rice")
 
 # 1. Instantiate a rice crop
-pass  # YOUR CODE HERE
+rice = Rice()
 
 # 2. Water the rice crop
-pass  # YOUR CODE HERE
+rice.water()
 
 # 3. Transplant the rice crop
-pass  # YOUR CODE HERE
+rice.transplant()
 
 # 4. Print "The rice crop produced ## grains"
-pass  # YOUR CODE HERE
+print(f"The rice crop produced {rice.grains} grains")
 
 # 5. Print "The rice crop is ripe" or "The rice crop is not ripe"
-pass  # YOUR CODE HERE
+if rice.ripe():
+    print("The rice crop is ripe")
+else:
+    print("The rice crop is not ripe")
